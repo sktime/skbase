1f86bbcf3c868d2749b60460acc068b9b3bed2cf
rnkuhns <rnkuhns@gmail.com>
2022-11-23 16:49:57 -0500

    Add type check to _is_non_public_module

--- skbase/lookup/_lookup.py
+++ skbase/lookup/_lookup.py
@@ -108,6 +108,10 @@ def _is_non_public_module(module_name: str) -> bool:
     is_non_public : bool
         Whether the module is non-public or not.
     """
+    if not isinstance(module_name, str):
+        raise ValueError(
+            f"Parameter `module_name` should be str, but found {type(module_name)}."
+        )
     is_non_public: bool = "._" in module_name or (
         "." not in module_name and module_name.startswith("_")
     )
