b20283bde83522f7d03711d28b1c150907a57714
rnkuhns <rnkuhns@gmail.com>
2023-03-13 01:25:47 -0400

    Add missing __init__

---
+++ skbase/base/_pretty_printing/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+# copyright: skbase developers, BSD-3-Clause License (see LICENSE file)
+# Many elements of this code were developed in scikit-learn. These elements
+# are copyrighted by the scikit-learn developers, BSD-3-Clause License. For
+# conditions see https://github.com/scikit-learn/scikit-learn/blob/main/COPYING
+"""Functionality for pretty printing BaseObjects."""
+from typing import List
+
+__author__: List[str] = ["RNKuhns"]
+__all__: List[str] = []
