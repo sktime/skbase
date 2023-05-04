1ccb2c5e74bb83886947eeb9873a9fec1242c46b
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-04 12:23:05 +0100

    move utils

---
+++ skbase/utils/dependencies/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+# copyright: skbase developers, BSD-3-Clause License (see LICENSE file)
+"""Utility functionality used through `skbase`."""
+
+from skbase.utils.dependencies._dependencies import (
+    _check_python_version,
+    _check_soft_dependencies,
+)
+
+__all__ = ["_check_python_version", "_check_soft_dependencies"]
