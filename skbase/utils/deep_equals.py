84073e87520201875d90eb13300dc0ed6cba68c7
Franz Király <f.kiraly@ucl.ac.uk>
2023-10-16 08:36:15 +0200

    docstr

--- skbase/utils/deep_equals.py
+++ skbase/utils/deep_equals.py
@@ -32,11 +32,29 @@ pandas_available = _softdep_available("pandas")
 
 
 def _ret(is_equal, msg="", string_arguments: list = None, return_msg=False):
+    """Return is_equal and msg, formatted with string_arguments if return_msg=True.
+
+    Parameters
+    ----------
+    is_equal : bool
+    msg : str, optional, default=""
+        message to return if is_equal=False
+    string_arguments : list, optional, default=None
+        list of arguments to format msg with
+
+    Returns
+    -------
+    is_equal : bool
+        identical to input ``is_equal``, always returned
+    msg : str, only returned if return_msg=True
+        if ``is_equal=True``, ``msg`` is always ``""``
+        if ``is_equal=False``, ``msg`` is formatted with ``string_arguments``
+        via ``msg.format(*string_arguments)``
+    """
     if return_msg:
         if is_equal:
             msg = ""
-        elif len(string_arguments) > 0:
-            string_arguments = [] if string_arguments is None else string_arguments
+        elif isinstance(string_arguments, (list, tuple)) and len(string_arguments) > 0:
             msg = msg.format(*string_arguments)
         return is_equal, msg
     else:
