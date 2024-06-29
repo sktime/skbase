ea1c0f5a549d7c7d55f0fe1ff80d876d014049eb
Franz Király <f.kiraly@ucl.ac.uk>
2024-06-29 15:16:12 +0100

    conftest

--- skbase/tests/conftest.py
+++ skbase/tests/conftest.py
@@ -252,6 +252,8 @@ SKBASE_FUNCTIONS_BY_MODULE.update(
             "_check_soft_dependencies",
             "_check_python_version",
             "_check_estimator_deps",
+            "_get_pkg_version",
+            "_normalize_requirement",
         ),
         "skbase.utils.random_state": (
             "check_random_state",
