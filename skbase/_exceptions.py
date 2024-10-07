2059c2b14907e5c4d924b7136bb51c5e1f455ce8
Franz Király <f.kiraly@ucl.ac.uk>
2024-10-07 15:52:57 +0100

    Update _exceptions.py

--- skbase/_exceptions.py
+++ skbase/_exceptions.py
@@ -21,11 +21,10 @@ class FixtureGenerationError(Exception):
 class NotFittedError(ValueError, AttributeError):
     """Exception class to raise if estimator is used before fitting.
 
-    This class inherits from both ValueError and AttributeError to help with
+    This class inherits from both ``ValueError`` and ``AttributeError`` to help with
     exception handling.
 
     References
     ----------
-    [1] scikit-learn's NotFittedError
-    [2] sktime's NotFittedError
+    .. [1] Based on scikit-learn's NotFittedError
     """
