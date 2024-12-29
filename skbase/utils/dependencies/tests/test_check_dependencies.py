c7a75bab7746e5887ffb9c2ce042f3d7b1d3d962
Luigi Giugliano <luigi3000@gmail.com>
2024-12-29 12:27:38 +0100

    [BUG] Accepting prereleases as valid python version (#389)
    #### Reference Issues/PRs
    Fixes: https://github.com/sktime/sktime/issues/7517
    
    
    Make "rc" python version recognizable during python version checking.
    Introducing a parameter `prereleases=True`
    The following code snipped now returns true:
    ```python
    "3.11.0rc1" in SpecifierSet(">3.8", prereleases=True) 
    ```

--- skbase/utils/dependencies/tests/test_check_dependencies.py
+++ skbase/utils/dependencies/tests/test_check_dependencies.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """Tests for _check_soft_dependencies utility."""
+from unittest.mock import patch
+
 import pytest
 from packaging.requirements import InvalidRequirement
 
-from skbase.utils.dependencies._dependencies import _check_soft_dependencies
+from skbase.utils.dependencies import _check_python_version, _check_soft_dependencies
 
 
 def test_check_soft_deps():
@@ -47,3 +49,53 @@ def test_check_soft_deps():
         assert _check_soft_dependencies(
             ("pytest", "!!numpy<~><>0.1.0"), severity="none"
         )
+
+
+@patch("skbase.utils.dependencies._dependencies.sys")
+@pytest.mark.parametrize(
+    "mock_release_version, prereleases, expect_exception",
+    [
+        (True, True, False),
+        (True, False, True),
+        (False, False, False),
+        (False, True, False),
+    ],
+)
+def test_check_python_version(
+    mock_sys, mock_release_version, prereleases, expect_exception
+):
+    from skbase.base import BaseObject
+
+    if mock_release_version:
+        mock_sys.version = "3.8.1rc"
+    else:
+        mock_sys.version = "3.8.1"
+
+    class DummyObjectClass(BaseObject):
+        _tags = {
+            "python_version": ">=3.7.1",  # PEP 440 version specifier, e.g., ">=3.7"
+            "python_dependencies": None,  # PEP 440 dependency strs, e.g., "pandas>=1.0"
+            "env_marker": None,  # PEP 508 environment marker, e.g., "os_name=='posix'"
+        }
+        """Define dummy class to test set_tags."""
+
+    dummy_object_instance = DummyObjectClass()
+
+    try:
+        _check_python_version(dummy_object_instance, prereleases=prereleases)
+    except ModuleNotFoundError as exception:
+        expected_msg = (
+            f"{type(dummy_object_instance).__name__} requires python version "
+            f"to be {dummy_object_instance.get_tags()['python_version']}, "
+            f"but system python version is {mock_sys.version}. "
+            "This is due to the release candidate status of your system Python."
+        )
+
+        if not expect_exception or exception.msg != expected_msg:
+            # Throw Error since exception is not expected or has not the correct message
+            raise AssertionError(
+                "ModuleNotFoundError should be NOT raised by:",
+                f"\n\t - mock_release_version: {mock_release_version},",
+                f"\n\t - prereleases: {prereleases},",
+                f"\nERROR MESSAGE: {exception.msg}",
+            ) from exception
