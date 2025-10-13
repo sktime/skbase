598aa45a9af8aae6fed6836335c1467048c77340
Simon Blanke <simon.blanke@yahoo.com>
2025-10-13 18:24:17 +0200

    fix test

--- skbase/tests/test_lookup_metaclasses.py
+++ skbase/tests/test_lookup_metaclasses.py
@@ -1,41 +1,25 @@
 # -*- coding: utf-8 -*-
 """Tests for lookup utilities handling classes defined with metaclasses."""
 
-from typing import List, Tuple
+from typing import List
 
 from skbase.lookup import all_objects, get_package_metadata
-from skbase.tests.metaclass_objects import MetaExample, MetaSubclass
 from skbase.utils.dependencies._import import CommonMagicMeta, MagicAttribute
 
 __author__: List[str] = ["SimonBlanke"]
 
 
-def _metaclass_ignore_modules() -> Tuple[str, ...]:
-    """Return modules we ignore to keep imports light in tests."""
-    return (
-        "test_base",
-        "test_baseestimator",
-        "test_meta",
-        "test_exceptions",
-        "test_tagaliaser",
-        "mock_package",
-        "conftest",
-        "test_lookup_metaclasses",
-    )
-
-
 def test_all_objects_discovers_metaclass_classes():
     """Classes defined with metaclasses should be returned by all_objects."""
     results = all_objects(
-        package_name="skbase.tests",
+        package_name="skbase.utils.dependencies._import",
         object_types=object,
-        modules_to_ignore=_metaclass_ignore_modules(),
+        modules_to_ignore=(),
         suppress_import_stdout=True,
     )
     objects = dict(results)
 
-    assert objects["MetaExample"] is MetaExample
-    assert objects["MetaSubclass"] is MetaSubclass
+    assert objects["MagicAttribute"] is MagicAttribute
     assert objects["CommonMagicMeta"] is CommonMagicMeta
 
 
