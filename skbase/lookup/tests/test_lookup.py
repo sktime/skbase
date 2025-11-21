72a688c1474fd1225cc9d551d1f0b9cd074e7b9d
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-21 23:41:51 +0530

    tests(lookup): restrict package_name to skbase.tests to reduce scan scope (fix issue #114)

--- skbase/lookup/tests/test_lookup.py
+++ skbase/lookup/tests/test_lookup.py
@@ -926,7 +926,7 @@ def test_all_objects_class_filter(class_filter):
     """Test all_objects filters by class type as expected."""
     # Results applying filter
     objs = all_objects(
-        package_name="skbase",
+        package_name="skbase.tests",
         return_names=True,
         as_dataframe=True,
         return_tags=None,
@@ -940,7 +940,7 @@ def test_all_objects_class_filter(class_filter):
 
     # Results without filter
     objs = all_objects(
-        package_name="skbase",
+        package_name="skbase.tests",
         return_names=True,
         as_dataframe=True,
         return_tags=None,
@@ -966,7 +966,7 @@ def test_all_object_tag_filter(tag_filter):
     """Test all_objects filters by tag as expected."""
     # Results applying filter
     objs = all_objects(
-        package_name="skbase",
+        package_name="skbase.tests",
         return_names=True,
         as_dataframe=True,
         return_tags=None,
@@ -980,7 +980,7 @@ def test_all_object_tag_filter(tag_filter):
 
     # Results without filter
     objs = all_objects(
-        package_name="skbase",
+        package_name="skbase.tests",
         return_names=True,
         as_dataframe=True,
         return_tags=None,
@@ -1041,7 +1041,7 @@ def test_all_object_class_lookup(class_lookup, class_filter):
     """Test all_objects class_lookup parameter works as expected.."""
     # Results applying filter
     objs = all_objects(
-        package_name="skbase",
+        package_name="skbase.tests",
         return_names=True,
         as_dataframe=True,
         return_tags=None,
@@ -1064,7 +1064,7 @@ def test_all_object_class_lookup_invalid_object_types_raises(
     # Results applying filter
     with pytest.raises(ValueError):
         all_objects(
-            package_name="skbase",
+            package_name="skbase.tests",
             return_names=True,
             as_dataframe=True,
             return_tags=None,
