d670fa2ab3265dbc7c601ebc4bbc46b2707081e9
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-22 13:37:09 +0530

    style(tests): fix indentation (replace tabs with spaces) in mock_package __init__

--- skbase/tests/mock_package/__init__.py
+++ skbase/tests/mock_package/__init__.py
@@ -7,14 +7,22 @@ from typing import List
 
 from .fixtures import Child, ClassWithABTrue, Parent
 from .test_mock_package import (
-    MOCK_PACKAGE_OBJECTS,
-    AnotherClass,
     CompositionDummy,
     InheritsFromBaseObject,
+    AnotherClass,
     NotABaseObject,
+    MOCK_PACKAGE_OBJECTS,
 )
 
 __all__: List[str] = [
+    "CompositionDummy",
+    "InheritsFromBaseObject",
+    "AnotherClass",
+    "NotABaseObject",
+    "MOCK_PACKAGE_OBJECTS",
+    "Parent",
+    "Child",
+    "ClassWithABTrue",
     "CompositionDummy",
     "InheritsFromBaseObject",
     "AnotherClass",
