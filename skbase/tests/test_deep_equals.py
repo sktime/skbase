22695ad74a197d64aef6df57d197b29cfee60957
Tobias Pitters <31857876+CloseChoice@users.noreply.github.com>
2026-06-20 18:02:45 +0200

    [ENH] early return for `deep_equals` in case of object identity (#568)
    This fixes the following problem
    
    
    ```python
        from pytorch_forecasting.metrics.quantile import QuantileLoss
        import numpy as np
    
        a = QuantileLoss(quantiles=[0.1, 0.2])
        assert deep_equals(a, a)  # Fails
    ```
    this fails since the `QuantileLoss` overwrites `__eq__` and `__ne__`. I
    simulate this in a test that fails on main as well, in order to not
    introduce another heavy dependency.

--- skbase/tests/test_deep_equals.py
+++ skbase/tests/test_deep_equals.py
@@ -1,8 +1,9 @@
+from skbase.utils.deep_equals import deep_equals
+
+
 def test_deep_equals_numpy_string_array():
     """Test that deep_equals works on string-dtype numpy arrays. GitHub issue #517."""
     import numpy as np
-
-    from skbase.utils.deep_equals import deep_equals
 
     s = np.array(["1", "2", "1"])
     # Same arrays should be equal
@@ -11,3 +12,24 @@ def test_deep_equals_numpy_string_array():
     # Different arrays should not be equal
     t = np.array(["1", "2", "3"])
     assert deep_equals(s, t) is False
+
+
+def test_deep_equals_equal_object():
+    class DummyComposite:
+        def __init__(self, func1, func2, neq_called):
+            self.func1 = func1
+            self.func2 = func2
+            self.neq_called = neq_called
+
+    class Dummy:
+        def __init__(self, func):
+            self.func = func
+
+        def __eq__(self, other) -> DummyComposite:
+            return DummyComposite(self.func, other.func, False)
+
+        def __ne__(self, other) -> DummyComposite:
+            return DummyComposite(self.func, other.func, True)
+
+    a = Dummy(lambda x: x)
+    assert deep_equals(a, a)
